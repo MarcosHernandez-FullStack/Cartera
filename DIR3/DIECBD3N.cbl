9dab7a7e9bbb0cd939d31c41cb5ec822e7e776db
MarcosHernandez-FullStack <145492017+MarcosHernandez-FullStack@users.noreply.github.com>
2026-08-14 15:49:46 -0500

    Create DIECBD3N.cbl

---
+++ DIR3/DIECBD3N.cbl
@@ -0,0 +1,20 @@
+      *****************************************************************
+      *                 FORMATO DE SALIDA BD3N                        *
+      *****************************************************************
+       01 BD3N-DIECBD3N.
+          10 BD3N-ARREGLO-MOVTOS.
+             15 BD3N-FORMATO             PIC X(08).
+             15 BD3N-FECHA-VENCIM        PIC X(10).
+             15 BD3N-NUM-DOCU            PIC X(10).
+             15 BD3N-NOM-ACEPTA          PIC X(40).
+             15 BD3N-INGRESO             PIC S9(15)V99.
+             15 BD3N-IMPORTE             PIC S9(15)V99.
+             15 BD3N-INTERES             PIC S9(15)V99.
+             15 BD3N-COMISIO             PIC S9(15)V99.
+             15 BD3N-GASTOS              PIC S9(15)V99.
+             15 BD3N-SITUACION           PIC X(03).
+             15 BD3N-NUM-ORIGINAL        PIC X(12).
+      *
+      ******************************************************************
+      *                     FIN COPY DIECBD3N                         **
+      ******************************************************************
